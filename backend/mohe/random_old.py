7db8479f0986db9b42d304ab62188b92f9f4d62e
Alexander Yang <65861156+blueputty01@users.noreply.github.com>
2024-04-21 01:41:54 -0400

    idek

--- backend/mohe/random_old.py
+++ backend/mohe/random_old.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import csv
+import random
 
 NEW = [
     "Best Public Sector Hack - Bloomberg Industry Group",
@@ -67,7 +68,6 @@ LOOKUP = {
     "Best Razzle Dazzle Hack - Bitcamp": SAME,
     "Bloomberg - Most Philanthropic Hack": "Most Philanthropic Hack - Bloomberg",
     "Most Creative Use of Twilio - Major League Hacking": SAME,
-    "Best Machine Learning Hack - Bitcamp": SAME,
     "Bloomberg Industry Group - Best Public Sector Hack": "Best Public Sector Hack - Bloomberg Industry Group",
     "Capital One - Best Financial Hack": "Best Financial Hack - Capital One",
     "Cockroach Labs - Best Use of CockroachDB Serverless": "Best Use of CockroachDB Serverless - Cockroach Labs",
@@ -78,45 +78,118 @@ LOOKUP = {
     "Best Use of MongoDB Atlas - Major League Hacking": SAME,
     "Best Social Good Hack - Bitcamp": SAME,
     "Best Blockchain Project Using Hedera - Major League Hacking": SAME,
-    "Advanced Quantum Track - Bitcamp": SAME,
     "People's Choice Hack - Bitcamp": SAME,
     "Best Hardware Hack - Bitcamp": SAME,
     "Best Gamification Hack - Bitcamp": SAME,
     "Cipher Tech - Best Digital Forensics Hack": "Best Digital Forensics Hack - Cipher Tech",
     "Best Bitcamp Hack - Bitcamp": SAME,
     "Best Accessibility Hack sponsored by Fidelity - Major League Hacking": SAME,
+    "Best Machine Learning Hack - Bitcamp": SAME,
+    "Advanced Quantum Track - Bitcamp": "Best Advanced Quantum Track Hack",
 }
 
 
-def parse_old(file):
+def print_cats(file):
     with open(file, "r") as f:
         data = list(csv.DictReader(f))
 
         # find and print all unique entries in the list represented as a string in column J of the CSV
-        # uniques = set()
-        # for row in data:
-        #     item_string = row["Opt-In Prizes"]
-        #     items = item_string.split(",")
-        #     for item in items:
-        #         item = item.strip()
-        #         uniques.add(item)
-        # print(uniques)
-
-        # replace all old names with new names
+        uniques = set()
         for row in data:
             item_string = row["Opt-In Prizes"]
             items = item_string.split(",")
-            for i, item in enumerate(items):
+            for item in items:
                 item = item.strip()
-                if item in LOOKUP:
-                    items[i] = LOOKUP[item]
-            row["Opt-In Prizes"] = ", ".join(items)
+                uniques.add(item)
+        print(uniques)
+
+
+OLD_TRACKS = [
+    "Best Machine Learning Hack - Bitcamp",
+    "Best Advanced Quantum Track Hack",
+]
+
+
+def rename_old_cats(file, out):
+    for item in LOOKUP:
+        if LOOKUP[item] == SAME:
+            LOOKUP[item] = item
+
+    with open(file, "r") as f:
+        data = list(csv.DictReader(f))
+
+        # replace all old names with new names
+        for row in data:
+            print(row["Opt-In Prizes"])
+            item_string = row["Opt-In Prizes"]
+
+            for item in LOOKUP:
+                if item in item_string:
+                    item_string = item_string.replace(item, LOOKUP[item])
+
+            items = item_string.split(",")
+
+            new_items = items.copy()
+            for item in enumerate(new_items):
+                item = item.strip()
+                if item in OLD_TRACKS:
+                    row["Bitcamp Track Challenge"] = item
+                    # remove the track from the list of items
+                    print("removing", item)
+                    new_items.remove(item)
+
+            # update the row with the new names
+            update = ", ".join(items)
+            # match anything in item_string that matches OLD_TRACKS and put it in "Bitcamp Track Challenge"
+            # for track in OLD_TRACKS:
+            #     if track in item_string:
+            #         row["Bitcamp Track Challenge"] = track
+            #         update = update.replace(track + ", ", "")
+
+            row["Opt-In Prizes"] = update
 
         # save the new data to a new file
-        with open("2023 bitcamp new.csv", "w") as f:
+        with open(out, "w") as f:
             writer = csv.DictWriter(f, fieldnames=data[0].keys())
             writer.writeheader()
             writer.writerows(data)
 
 
-parse_old("2023 bitcamp.csv")
+TRACKS = [
+    # "Best Machine Learning Track Hack - Bitcamp",
+    "Best Cybersecurity Track Hack - Bitcamp",
+    "Best App Dev Track Hack - Bitcamp",
+    "Beginner Quantum Track Hacks - Bitcamp",
+    # "Best Advanced Quantum Track Hack",
+]
+
+
+def randomize_old_cats(file, out):
+    with open(file, "r") as f:
+        data = list(csv.DictReader(f))
+
+        # if empty, pick a randomized category
+        for row in data:
+            item_string = row["Bitcamp Track Challenge"]
+            if item_string == "":
+                # generate random number between 0 and 5
+                random_index = random.randint(0, 100)
+                # PARAMS: 20, 40, 60, 80
+                if random_index < 20:
+                    row["Bitcamp Track Challenge"] = TRACKS[0]
+                elif random_index < 40:
+                    row["Bitcamp Track Challenge"] = TRACKS[1]
+                elif random_index < 60:
+                    row["Bitcamp Track Challenge"] = TRACKS[2]
+                else:
+                    row["Bitcamp Track Challenge"] = ""
+
+        # save the new data to a new file
+        with open(out, "w") as f:
+            writer = csv.DictWriter(f, fieldnames=data[0].keys())
+            writer.writeheader()
+            writer.writerows(data)
+
+
+rename_old_cats("2023 bitcamp.csv", "2023 bitcamp filtered.csv")
+randomize_old_cats("2023 bitcamp filtered.csv", "2023 bitcamp FINAL.csv")
