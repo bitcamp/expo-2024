c8efcee30491143e26973c2452bb4e68e9f605f6
Eric Chen <ericchen314@gmail.com>
2024-04-21 10:08:20 -0400

    Make judging group counts

--- backend/parse.py
+++ backend/parse.py
@@ -14,7 +14,7 @@ from collections import defaultdict
 from datetime import datetime, timedelta
 import pytz
 
-random.seed(10)
+# random.seed(10)
 
 DRAFT_COLUMN_NAME = 'Project Status'
 TEAM_COLUMN_NAME = 'Project Title'
@@ -62,6 +62,41 @@ BITCAMP_HACKS = set([
     "People's Choice Hack - Bitcamp",
     "Best Sustainability Hack - Bitcamp"
 ])
+
+EXCLUDED_CHALLENGES = set([
+    "People's Choice Hack - Bitcamp",
+    "Beginner Quantum Track Hacks - Bitcamp"
+])
+
+CHALLENGE_JUDGE_GROUPS = [
+    3, #"Best Machine Learning Track Hack - Bitcamp", (3+4)
+    4, #"Best App Dev Track Hack - Bitcamp", (11+2)
+    2, #"Best Cybersecurity Track Hack - Bitcamp", (3+2)
+    0, #"Beginner Quantum Track Hacks - Bitcamp", (4+2)
+    3, #"Best Advanced Quantum Track Hack - Bitcamp", (4+2)
+
+    2, #"Best Hardware Hack - Bitcamp",
+    2, #"Best Bitcamp Hack - Bitcamp",
+    2, #"Best First Time Hack - Bitcamp",
+    2, #"Best UI/UX Hack - Bitcamp",
+    2, #"Best Moonshot Hack - Bitcamp",
+    2, #"Best Razzle Dazzle Hack - Bitcamp",
+    2, #"Best Social Good Hack - Bitcamp",
+    2, #"Best Gamification Hack - Bitcamp",
+    2, #"People's Choice Hack - Bitcamp",
+    2, #"Best Sustainability Hack - Bitcamp",
+
+    2, #"Best use of AI/ML Innovation for the Francis Scott Key Bridge Recovery Efforts - Cloudforce",
+    2, #"Most Philanthropic Hack - Bloomberg",
+    1, #"Best Digital Forensics Related Hack - Cipher Tech",
+    2, #"Best Use of APIs related to Housing/Climate Change - Fannie Mae",
+    2, #"Best AI Powered Solution for Defense Contracts - Bloomberg Industry Group", 
+    2, #"Best Financial Hack - Capital One",
+    2 #"University Course Catalog Data Extraction and Query Challenge - Xficient",
+]
+
+TABLES = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'I1', 'I2', 'I3', 'I4', 'I5', 'I6', 'I7', 'I8', 'J1', 'J2', 'J3', 'J4', 'J5', 'J6', 'J7', 'J8', 'K1', 'K2', 'K5', 'K6', 'K7', 'K8', 'L1', 'L2', 'L5', 'L6', 'L7', 'L8', 'M1', 'M2', 'M3', 'M4', 'M5', 'M6', 'M7', 'M8', 'N1', 'N2', 'N3', 'N4', 'N5', 'N6', 'N7', 'N8', 'O1', 'O2', 'O3', 'O4', 'O5', 'O6', 'O7', 'O8', 'P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P7', 'P8', 'Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q7', 'Q8', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6', 'R7', 'R8', 'S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'T1', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7', 'T8']
+random.shuffle(TABLES)
 
 FULL_CHALLENGE_LIST = [
     "Best Machine Learning Track Hack - Bitcamp",
@@ -229,7 +264,7 @@ def process_challenges(challenges):
             tc = tc.strip()
             if tc in MLH_HACKS:
                 current_mlh_challenges.append(tc)
-            elif len(tc) > 0 and tc != "People's Choice Hack - Bitcamp":
+            elif len(tc) > 0 and (tc not in EXCLUDED_CHALLENGES):
                 current_challenges.append(tc)
         result.append(current_challenges)
         MLH_challenges.append(current_mlh_challenges)
@@ -313,7 +348,7 @@ def expo_output_to_json(t, H, team_names, links, in_person_list, MLH_challenges)
         team_json = {
             "id": id,
             "team_name": team_names[id],
-            "table": "A1" if in_person_list[id] else "virtual",
+            "table": TABLES[0] if in_person_list[id] else "virtual",
             "in_person": in_person_list[id],
             "link": links[id],
         }
